d01fc5f9dd76f883f07bee92b1b44af7ef47d8fd
paulm6438 <paulm6438@HRH-IC0027320.wrdsb.ca>
2015-10-01 10:56:06 -0400

    initial

--- src/Quiz2.java
+++ src/Quiz2.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -6,7 +9,7 @@
 
 /**
  *
- * @author PUT_YOUR_NAME_HERE
+ * @author Mark
  */
 public class Quiz2 {
 
@@ -19,7 +22,14 @@ public class Quiz2 {
      * @return 
      */
     public int sumUpTo(int n){
-        
+        if(n==0)
+        {
+            return 0;
+        }else
+        {
+            int row = n;
+            return sumUpTo(n-1) + row;
+        }
     }
     
     /**
@@ -29,8 +39,16 @@ public class Quiz2 {
      * @return the input word reversed
      */
     public String reverseString(String word){
-        
-    }
+        if(word.length() < 1){
+            return null;
+        }else 
+            {
+                String letter = word.charAt(0);
+                return reverseString(word.length()word.charAt(0)-1,letter);
+            }
+        //return isPalindrome(s.substring(1,s.length() -1));
+        return null;
+     }
     
     
     /**
@@ -39,7 +57,8 @@ public class Quiz2 {
     public static void main(String[] args) {
         // Use this section for conducting tests
         Quiz2 test = new Quiz2();
-        
+
+        //System.out.println(test.sumUpTo(n));
         //use test.sumUpTo(__)  or test.reverseString(___) to test
         
     }
