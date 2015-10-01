3539f66af1ee5ce29deffc3e9423e9c5113405d1
muirw5809 <muirw5809@HRH-IC0027321.wrdsb.ca>
2015-10-01 11:12:18 -0400

    this is the initial commit

--- src/Quiz2.java
+++ src/Quiz2.java
@@ -20,6 +20,17 @@ public class Quiz2 {
      */
     public int sumUpTo(int n){
         
+       
+        if(n == 0)                          //if the number inputed is 0
+        {
+            return 0;                       //return 0    
+        }else if(n==1)                      //if the number inputed is 1              
+        {
+            return 1;                       //return 1
+        }else{                              //if the number inputed is greater than 1
+            return n + sumUpTo(n-1);        //add the number, subtract 1 from the number and add it again 
+        }                                   //until the number equals 0
+        
     }
     
     /**
@@ -30,6 +41,24 @@ public class Quiz2 {
      */
     public String reverseString(String word){
         
+        int n = word.length() -1;                   //n equals the last letter
+        
+        
+     char first = word.charAt(0);                  //first equals the first letter of the word 
+     char last = word.charAt(word.length()-1);     //last equals the last letter of the word
+        if(n <=1){                                 //if the word length is 1 letter or no letters 
+            return word;                           //return the word as it is
+        
+        }else if(n>1){                             //if the word as 2 or more letters
+           
+            first = word.charAt(0);                //first still equals the first letter
+            last = word.charAt(n);                 //last still equals the last letter
+          word = word.substring(1,n);              //the new word equals the first and the last letters flipped
+            
+        }return last+ reverseString(word)+first;   //return the last letter
+                                                   //than the rest of the original word
+                                                   //then the first letter after
+                                                   //and keep repeating the process until n equals 1
     }
     
     
@@ -40,6 +69,8 @@ public class Quiz2 {
         // Use this section for conducting tests
         Quiz2 test = new Quiz2();
         
+        System.out.println(test.sumUpTo(5)); 
+        System.out.println(test.reverseString("doggie"));
         //use test.sumUpTo(__)  or test.reverseString(___) to test
         
     }
